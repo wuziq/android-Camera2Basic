32ac7a5b08eeeb5173a2aaf78c0bef8e496c763d
wuziq <spamziq@gmail.com>
2017-05-07 23:28:36 -0700

    get and log capture capabilities

--- Application/src/main/java/com/example/android/camera2basic/CameraActivity.java
+++ Application/src/main/java/com/example/android/camera2basic/CameraActivity.java
@@ -17,14 +17,83 @@
 package com.example.android.camera2basic;
 
 import android.app.Activity;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class CameraActivity extends Activity {
+
+    private static final String TAG = CameraActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        CameraManager cameraManager = (CameraManager)getSystemService( CAMERA_SERVICE );
+        try
+        {
+            String cameraList[] = cameraManager.getCameraIdList();
+            for ( String cameraId : cameraList )
+            {
+                Log.e( TAG, "Camera id " + cameraId + " has device level:" );
+                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics( cameraId );
+                Integer deviceLevel = cameraCharacteristics.get( CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL );
+                if ( null != deviceLevel )
+                {
+                    switch ( deviceLevel )
+                    {
+                    case CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_3:
+                        Log.e( TAG, "INFO_SUPPORTED_HARDWARE_LEVEL_3" );
+                        break;
+                    case CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_FULL:
+                        Log.e( TAG, "INFO_SUPPORTED_HARDWARE_LEVEL_FULL" );
+                        break;
+                    case CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_LIMITED:
+                        Log.e( TAG, "INFO_SUPPORTED_HARDWARE_LEVEL_LIMITED" );
+                        break;
+                    case CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_LEGACY:
+                        Log.e( TAG, "INFO_SUPPORTED_HARDWARE_LEVEL_LEGACY" );
+                        break;
+                    }
+                }
+                else
+                {
+                    Log.e( TAG, "null" );
+                }
+
+                Log.e( TAG, "Camera id " + cameraId + " has the following request keys available:" );
+                List<CaptureRequest.Key<?>> captureRequestKeys = cameraCharacteristics.getAvailableCaptureRequestKeys();
+                for ( CaptureRequest.Key<?> key : captureRequestKeys )
+                {
+                    Log.e( TAG, key.getName() );
+                }
+
+                Log.e( TAG, "Camera id " + cameraId + " has the following capabilities available:" );
+                int availableCapabilities[] = cameraCharacteristics.get( CameraCharacteristics.REQUEST_AVAILABLE_CAPABILITIES );
+                if ( null != availableCapabilities )
+                {
+                    for ( int availableCapability : availableCapabilities )
+                    {
+                        Log.e( TAG, String.valueOf( availableCapability ) );
+                    }
+                }
+                else
+                {
+                    Log.e( TAG, "none." );
+                }
+            }
+        }
+        catch ( CameraAccessException e )
+        {
+            e.printStackTrace();
+        }
+
         if (null == savedInstanceState) {
             getFragmentManager().beginTransaction()
                     .replace(R.id.container, Camera2BasicFragment.newInstance())
